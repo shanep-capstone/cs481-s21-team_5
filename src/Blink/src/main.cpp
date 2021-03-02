4037d35ea876181f89bb147a8bd8f271a9f6e12e
gfegler <ginafegler@u.boisestate.edu>
2021-03-02 10:22:30 -0700

    fixed build

--- src/Blink/src/main.cpp
+++ src/Blink/src/main.cpp
@@ -4,7 +4,7 @@
  * Turns on an LED on for one second,
  * then off for one second, repeatedly.
  */
-//#include "Arduino.h"
+#include "Arduino.h"
 
 // Set LED_BUILTIN if it is not defined by Arduino framework
 // #define LED_BUILTIN 13
